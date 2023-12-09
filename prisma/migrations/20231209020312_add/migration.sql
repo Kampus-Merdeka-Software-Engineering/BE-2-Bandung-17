21209f789a03d4a7e62431e5ed2ca0d7c12a3291
diahayuanggraeni <diahayuanggraeni18@gmail.com>
2023-12-09 10:03:39 +0700

    tes

--- prisma/migrations/20231209020312_add/migration.sql
+++ prisma/migrations/20231209020312_add/migration.sql
@@ -1,20 +1,30 @@
 -- CreateTable
-CREATE TABLE `Product` (
+CREATE TABLE `Catalog` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `price` INTEGER NOT NULL,
-    `imageUrl` VARCHAR(191) NULL,
-    `catalogId` INTEGER NULL,
+    `name` VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Catalog` (
+CREATE TABLE `Product` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `price` INTEGER NULL,
+    `imageUrl` VARCHAR(191) NULL,
+    `catalogId` INTEGER NULL,
+    `description` TEXT NOT NULL,
 
     PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `contact` (
+    `name` VARCHAR(255) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `message` TEXT NOT NULL,
+
+    UNIQUE INDEX `contact_name_key`(`name`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
