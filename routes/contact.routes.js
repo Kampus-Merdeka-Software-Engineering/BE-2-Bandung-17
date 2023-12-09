2e5e4523d422b32a061d4668fc75a8e5eea1536a
diahayuanggraeni <diahayuanggraeni18@gmail.com>
2023-12-10 00:58:18 +0700

    contact

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -3,22 +3,19 @@ const contactRoutes = express.Router();
 const { prisma } = require("../config/prisma")
 
 contactRoutes.post("/", async (req, res) => {
-    const { name, address, message } = req.body;
-    if (!name || !address || !message) {
-        res.status(400).json({ message: `Name, address, and message are required fields` });
-        return;
-    }
-    const newContact = await prisma.contact.create({
-        data: {
-            name,
-            address,
-            message,
-        },
-    });
-    res.status(201).json({
-        message: `Contact request submitted successfully`,
+    try {
+      const newContact = await prisma.contact.create({
+        data: req.body,
+      });
+   
+      res.status(201).json({
+        message: "Message created",
         data: newContact,
-    });
-});
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 
 module.exports = { contactRoutes };
